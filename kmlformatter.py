c9b375c7293f155f507b5f64279b2d7f5258b674
tristan <55862534+blkagst@users.noreply.github.com>
2020-06-20 14:50:45 +0000

    Add files via upload
    better formatting, auto dump database

--- kmlformatter.py
+++ kmlformatter.py
@@ -1,34 +1,46 @@
 #!/bin/python3
 
-def placemarkSkeleton(id, lon, lat, networks, time):
+import datetime
+import sqlite3
+
+
+def placemarkSkeleton(_id, lon, lat, networks, time):
     data = f"""
   <Placemark>
     <name>{id}</name>
-    <description>{networks}</description> 
+    <description>{networks}</description>
     <Point>
       <coordinates>{lon},{lat},0</coordinates>
     </Point>
   </Placemark>"""
     return data
 
-def main():
-    file = open("data.kml","w")
-    with open("data.sql") as f:
-        data = f.readlines()
+def writeToFile(file, data):
     start = """
 <?xml version="1.0" encoding="UTF-8"?>
 <kml xmlns="http://www.opengis.net/kml/2.2">
   <Document>"""
     file.write(start.lstrip())
+
     for line in data:
-        id, lon, lat, networks, time = line.rstrip().split('|')
-        file.write(placemarkSkeleton(id, lon, lat, networks, time))
-    end = """  
+        _id, lon, lat, networks, time = line.rstrip().split('|')
+        file.write(placemarkSkeleton(_id, lon, lat, networks, time))
+    end = """
   </Document>
 </kml>"""
     file.write(end)
     file.close()
 
+def main():
+    file = open("/home/pi/Documents/data.kml", "w")
+    conn = sqlite3.connect('/home/pi/Documents/sensordata.db')
+    conn.execute('.output /home/pi/Documents/data.kml')
+    day = 'a' + str(datetime.datetime.now().date()).replace('-', '')
+    conn.execute('select * from {};'.format(day))
+    conn.close()
+    with open("/home/pi/Documents/data.sql") as f:
+        data = f.readlines()
+    writeToFile(file, data)
+
 if __name__ == '__main__':
     main()
-
